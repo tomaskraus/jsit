f80248a422734047bfc4587df38675d1094527fe
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-04-05 05:48:18 +0200

    better error handling
    handles the missing file to test
    handles name clash when module require fails

--- main.js
+++ main.js
@@ -3,9 +3,10 @@
 const core = require('./core')
 
 //place all impure functions under this
-const impure = {}
+const impure = { error: false }
 
 impure.errAndExit = msg => {
+    impure.error = true
     console.error(msg)
     // process.exit(1)
     process.exitCode = 1    //soft exit, allowing things to close itself properly
@@ -23,20 +24,22 @@ impure.sandbox = (pathForModuleRequire) => {
     const requireFileStr = `var ${moduleName} = require("${fileName}")`
     console.log(`BEGIN | Module | ${moduleName} | File | ${fileName}`)
 
+    const outerImpure = impure
     try {
         eval(requireFileStr)
         return { eval: str => eval(str) }
     } catch (e) {
-        impure.errAndExit(e.message)
-        return { eval:null }
+        outerImpure.errAndExit(e)
+        return { eval: null }
     }
 }
+
+
+const fs = require('fs');
 
 impure.app = (filename, evaluationCallback, context) => {
     try {
         context.fileName = fileName
-
-        const fs = require('fs');
         const rs = fs.createReadStream(filename)
         rs.on('error', err => impure.errAndExit(err.message))
 
@@ -64,8 +67,16 @@ const path = require('path')
 const fileName = path.resolve(process.argv[2])
     .replace(/\\/g, "/")    //on Windows
 
-const sandB = impure.sandbox(fileName)
-impure.app(fileName, sandB.eval, core.context)
+//check if file exists
+try {
+    fs.accessSync(fileName, fs.constants.R_OK);
+    const sandB = impure.sandbox(fileName)
+    if (!impure.error) {
+        impure.app(fileName, sandB.eval, core.context)
+    }
+} catch (err) {
+    impure.errAndExit(err.message)
+}
 
 //----------------------------------------------------------------------------------------------
 
