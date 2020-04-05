907de19830d468842133e652247718236452aa69
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-04-05 03:55:11 +0200

    sandboxed eval of the tested code
    isolate evaluation logic from line processing

--- core.js
+++ core.js
@@ -15,28 +15,45 @@ context = {
     }
 }
 
-preprocessLine = (line, context) => {
-    line = parseLine(line, context)
-    line = filterLine(line, context)
-    line = isolateLine(line, context)
+preprocessLine = line => {
+    line = filterLine(line)
+    line = isolateLine(line)
     return line
+}
+
+const impure = {}
+
+//processLine :: (string -> boolean) -> string -> context -> context
+impure.processLine = (evaluationCallback, line, ctx) => {
+    ctx.lineNumber++
+    ctx.lineText = line
+    line = preprocessLine(line)
+
+    if (line.trim() !== "") {
+        try {
+            result = evaluationCallback(line)
+            ctx.stats.totalCount++
+            if (result == false) {
+                ctx.stats.failCount++
+                console.log(`FAIL | Line | ${ctx.lineNumber} | Is | ${result} | Should be | - | File | ${ctx.fileName} | Text | ${ctx.lineText}`)
+            }
+        } catch (e) {
+            console.error(`ERROR | Line | ${ctx.lineNumber} | File | ${ctx.fileName} | Exception | ${e} | Text | ${ctx.lineText}`)
+        }
+    }
+
+    return ctx
 }
 
 // ======================================================================================================
 
 
-const parseLine = (line, ctx) => {
-    ctx.lineNumber++
-    ctx.lineText = line
-    return line
-}
-
-const filterLine = (line, ctx) => {
+const filterLine = line => {
     let re = /^\s*\*:/
     return re.test(line) ? line : ""
 }
 
-const isolateLine = (line, ctx) => {
+const isolateLine = line => {
     let re = /^(\s*\*:)\s(.*$)/
     return line.replace(re, "$2")
 }
@@ -48,6 +65,6 @@ const isolateLine = (line, ctx) => {
 
 module.exports = {
     context,
-    preprocessLine,
+    impure,
 }
 
