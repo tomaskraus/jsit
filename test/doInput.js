89fcabd3f2454ad05131e9b34fda71953916ae9c
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-10-28 20:40:04 +0100

    introduce the Task type usage

--- test/doInput.js
+++ test/doInput.js
@@ -5,28 +5,46 @@ const RxOp = require('rxjs/operators')
 const { streamToStringRx } = require('rxjs-stream')
 const fs = require('fs')
 const split = require('split')
+const Task = require('data.task')
 
 
-const fileName = process.argv[2] || null
 
-const readStream = fileName == null ?
-    process.stdin
+
+const doWork = (stream) => {
+    const readStreamLines = stream.pipe(split())
+    const ob = streamToStringRx(readStreamLines)
+        .pipe(
+            RxOp.map(text => `//-- ${text}`)
+        )
+
+    ob.subscribe({
+        next: s => console.log(s.toUpperCase()),
+
+        complete: s => {
+            stream.destroy()
+            console.log('---end---')
+        }
+        ,
+    })
+
+}
+
+
+const getStreamFromFileName = path => {
+    return new Task((reject, resolve) => {
+        const readStream = fs.createReadStream(path, { encoding: 'utf8' })
+        readStream.on('open', _ => resolve(readStream)) //we want a stream object, not its data
+        readStream.on('error', error => reject(error))
+    })
+}
+
+
+let fileName = process.argv[2] || null
+fileName == null ?
+    doWork(process.stdin)
     :
-    fs.createReadStream(fileName, { encoding: 'utf8' })
-const readStreamLines = readStream.pipe(split())
-
-const ob = streamToStringRx(readStreamLines)
-    .pipe(
-        RxOp.map(text => `//-- ${text.toUpperCase()}`)
-    )
-
-
-ob.subscribe({
-    next: s => console.log(s.toUpperCase()),
-
-    complete: s => {
-        readStream.destroy()
-        console.log('---end---')
-    }
-    ,
-})
+    getStreamFromFileName(fileName)
+        .fork(
+            error => console.log(error.message),
+            data => doWork(data)
+        )
