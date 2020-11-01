abc3bb6ee286c8f92928a7c82bb702cf49a7d14e
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-11-01 05:45:07 +0100

    better, less cluttered messages
    only comments in successful tests

--- messagers/defaultMessager.js
+++ messagers/defaultMessager.js
@@ -3,20 +3,28 @@
  * 
  */
 
+const lineCommentAtTheEnd = line => line.replace(/^(.*)\/\/(.*)$/, "$2")
+const location = ctx => `${ctx.fileName}:${ctx.lineNum}`
+
+
 const Msg = {
     header: data => console.log(`START | file: [${data.fileName}] , module: [${data.moduleName}]`),
 
-    describe: ctx => console.log(`${ctx.line}`),
+    describe: ctx =>
+        console.log(
+            // `[${location(ctx)}] ${ctx.line}`
+            `${ctx.line}`
+        ),
     testOk: ctx =>
         console.log(
-            `OK | ${ctx.lineNum} | '${ctx.line}'`
+            `OK | '${lineCommentAtTheEnd(ctx.line)}'`
             // {
             //     type: 'Ok',
             //     context: ctx
             // }
         ),
     testFailure: ctx => console.log(
-        `FAIL | ${ctx.lineNum} | message: [${ctx.msg}] | '${ctx.line}'`
+        `FAIL | at: [${location(ctx)}] | message: [${ctx.msg}] | '${ctx.line}'`
         // {
         //     type: 'Failure',
         //     context: ctx
@@ -29,11 +37,12 @@ const Msg = {
     }),
     summary: ctx => console.log(
         `END | failed tests: [${ctx.stats.numFailed}] | total tests: [${ctx.stats.totalTests}]`
+        // ,
         // {
         //     type: 'Summary',
         //     context: ctx
         // }
-        ),
+    ),
 }
 
 
