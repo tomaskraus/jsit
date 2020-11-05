0bc5d1ebc158731ecb395441e4abf9942620a742
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-11-05 04:59:01 +0100

    isolate TestBlock
    to provide test lines for general usage

---
+++ TestBlock.js
@@ -0,0 +1,80 @@
+/**
+ * test block parser
+ * - filters test-block lines
+ * - tells whether a test-block is inside block-comment
+ */
+
+const { compose } = require('folktale/core/lambda')
+const { map, chain } = require('pointfree-fantasy')
+const tbf = require('./text-block-filter')
+
+
+const trimStr = s => s.trim()
+const isLineComment = s => tbf.Regex.JSLineComment.test(s)
+
+
+const removeLineComment = line => line.replace(/^(\/\/)(.*)$/, "$2")
+const repairTestHeader = line => line.replace(/^:::(.*)$/, "//:::$1")
+const lineCommentResulter = compose.all(
+    map(tbf.contextOverLine(
+        compose.all(
+            repairTestHeader, //restore the test header we've stripped earlier
+            trimStr,
+            removeLineComment
+        )
+    )),
+    tbf.resulterFilterLine(isLineComment),
+)
+
+
+const commentBlockParser = tbf.BlockParser.create(
+    tbf.blockBoundaryCreate(tbf.Regex.JSBlockCommentBegin, tbf.Regex.JSBlockCommentEnd),
+    tbf.blockCallbacksCreate(
+        ctx => tbf.Result.Error(ctx),
+        ctx => tbf.Result.Error(ctx)
+    ),
+    'cBlock'
+)
+
+const removeBlockCommentStar = line => line.replace(/(\s)*\*(.*)$/, "$1$2")
+const blockCommentResulter = compose.all(
+    map(tbf.contextOverLine(
+        compose(
+            trimStr,
+            removeBlockCommentStar
+        )
+    )),
+    commentBlockParser.resulterFilter,
+)
+
+
+class TestBlock {
+
+    constructor(blockCallbacks) {
+        this.parser = tbf.BlockParser.create(
+            tbf.blockBoundaryCreate(/^\/\/:::/, tbf.Regex.blankLine),
+            blockCallbacks,
+            'tBlock'
+        )
+    }
+
+    static create(blockCallbacks) {
+        return new TestBlock(blockCallbacks)
+    }
+
+    resulter = ctx => compose.all(
+        chain(this.parser.resulterFilter),
+        result => result.orElse(
+            lineCommentResulter
+        ),
+        blockCommentResulter,
+        tbf.contextOverLine(trimStr),
+    )(ctx)
+}
+
+
+//==================================================================================
+
+module.exports = {
+    TestBlock,
+}
--- TestRunner.js
+++ TestRunner.js
@@ -4,6 +4,7 @@ const { map, chain } = require('pointfree-fantasy')
 const L = require('lenses')
 
 const tbf = require('./text-block-filter')
+const tb = require('./TestBlock')
 
 
 // lenses   for evaluation-param 
@@ -16,50 +17,12 @@ const createContext = (fileName, originalContext) => (
     { ...originalContext, fileName, stats: { numFailed: 0, totalTests: 0 } }
 )
 
-
-const trimStr = s => s.trim()
 const isLineComment = s => tbf.Regex.JSLineComment.test(s)
-
-
-const removeLineComment = line => line.replace(/^(\/\/)(.*)$/, "$2")
-const repairTestHeader = line => line.replace(/^:::(.*)$/, "//:::$1")
-const lineCommentResulter = compose.all(
-    map(tbf.contextOverLine(
-        compose.all(
-            repairTestHeader, //restore the test header we've stripped earlier
-            trimStr,
-            removeLineComment
-        )
-    )),
-    tbf.resulterFilterLine(isLineComment),
-)
-
-
-const commentBlockParser = tbf.BlockParser.create(
-    tbf.blockBoundaryCreate(tbf.Regex.JSBlockCommentBegin, tbf.Regex.JSBlockCommentEnd),
-    tbf.blockCallbacksCreate(
-        ctx => tbf.Result.Error(ctx),
-        ctx => tbf.Result.Error(ctx)
-    ),
-    'cBlock'
-)
-
-const removeBlockCommentStar = line => line.replace(/(\s)*\*(.*)$/, "$1$2")
-const blockCommentResulter = compose.all(
-    map(tbf.contextOverLine(
-        compose(
-            trimStr,
-            removeBlockCommentStar
-        )
-    )),
-    commentBlockParser.resulterFilter,
-)
 
 
 const TestRunner = (messager, evaluator) => {
 
-    const testBlockParser = tbf.BlockParser.create(
-        tbf.blockBoundaryCreate(/^\/\/:::/, tbf.Regex.blankLine),
+    const testBlock = tb.TestBlock.create(
         tbf.blockCallbacksCreate(
             ctx => compose.all(
                 _resetVarsHandler,
@@ -67,29 +30,19 @@ const TestRunner = (messager, evaluator) => {
             )(ctx),
 
             tbf.Result.Error,
-        ),
-        'tBlock'
-    )
-
-
-    const allTestLinesResulter = compose.all(
-        chain(testBlockParser.resulterFilter),
-        result => result.orElse(
-            lineCommentResulter
-        ),
-        blockCommentResulter,
-        tbf.contextOverLine(trimStr),
+        )
     )
 
 
     const executableTestLinesResulter = compose.all(
         chain(tbf.resulterFilterLine(s => !isLineComment(s))),
-        allTestLinesResulter,
+        testBlock.resulter,
     )
 
 
     const inc = x => x + 1
 
+    
     const evaluate = () => {
         const resultAddFail = (ctx, err) => compose.all(
             tbf.tap(messager.testFailure),
@@ -148,7 +101,7 @@ const TestRunner = (messager, evaluator) => {
 
     return {
         reducer: testingReducer,
-        flush: testBlockParser.contextFlush,
+        flush: testBlock.parser.contextFlush,
         createContext: fileName => createContext(fileName, tbf.contextCreate()),
     }
 
