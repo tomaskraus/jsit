1ecf0a3e2ee81cc246f15adbe82387a8555274c5
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-11-06 06:45:52 +0100

    add switch-test tool
    can (un)comment in-line tests

---
+++ tools/switch-tests.js
@@ -0,0 +1,116 @@
+const { compose } = require('folktale/core/lambda')
+const { map } = require('pointfree-fantasy')
+const RxOp = require('rxjs/operators')
+
+const flt = require('../file-line-tools')
+const tbf = require('../text-block-filter')
+
+const { TestBlock } = require('../TestBlock')
+
+
+const CommentMode = { comment: 'comment', uncomment: 'uncomment' }
+const isCommentMode = str => str === CommentMode.comment
+
+const Marks = {
+    startBlockStr: { comment: '//*<+', uncomment: '<-*/' },
+    endBlockStr: { comment: '//>+*', uncomment: '/*>-' },
+    startBlockRegex: {},//{ comment: '*+/', uncomment: '-*/'},
+    endBlockRegex: {},//{ comment: '/+*', uncomment: '/*-'},
+
+    startLineStr: { comment: '////<+', uncomment: '////<-' },
+    endLineStr: { comment: '////>+', uncomment: '////>-' },
+    startLineRegex: {},//{ comment: '*+/', uncomment: '-*/'},
+    endLineRegex: {},//{ comment: '/+*', uncomment: '/*-'},
+}
+
+const indentStr = "    "
+
+const work = (stream, fileName, commentMode) => {
+
+    const testBlock = TestBlock.create(
+        compose.all(
+            tbf.Result.Ok, //we also want to show headers
+            tbf.tap(c => TestBlock.isInCommentBlock(c)
+                ? console.log(Marks.startBlockStr[commentMode])
+                : console.log(Marks.startLineStr[commentMode])
+            ),
+        ),
+
+        compose.all(
+            tbf.Result.Error,
+            tbf.tap(c => TestBlock.isInCommentBlock(c)
+                ? console.log(Marks.endBlockStr[commentMode])
+                : console.log(Marks.endLineStr[commentMode])
+            ),
+        )
+    )
+
+
+    const removeLineComment = line => line.replace(/^(\/\/)(.*)$/, "$2")
+    const repairUncommentedTestHeader = line => line.replace(/^:::(.*)$/, "//:::$1")
+
+    const addLineComment = line => line.replace(/^(.*)$/, "//$1")
+    const repairOvercommentedTestHeader = line => line.replace(/^\/\/\/\/:::(.*)$/, "//:::$1")
+
+    commentAction = tbf.contextOverLine(
+        compose(repairOvercommentedTestHeader, addLineComment)
+    )
+
+    uncommentAction = tbf.contextOverLine(
+        compose(repairUncommentedTestHeader, removeLineComment)
+    )
+
+    const allResulter = compose.all(
+        res => res.orElse(
+            c => tbf.Result.Error(
+                tbf.tap(cs => console.log(`${cs.original}`), c)
+            )
+        ),
+        map(tbf.tap(c => console.log(`${indentStr}${c.line}`))),
+        map(c => (!isCommentMode(commentMode) && !TestBlock.isInCommentBlock(c))
+            ? uncommentAction(c)
+            : flt.identity(c)
+        ),
+        map(c => (isCommentMode(commentMode) && !TestBlock.isInCommentBlock(c))
+            ? commentAction(c)
+            : flt.identity(c)
+        ),
+        testBlock.resulter,
+    )
+
+
+    // console.log(`processing: ${fileName}`)
+
+    flt.lineObservableFromStream(stream)
+        .pipe(
+            RxOp.scan(tbf.reducer(allResulter), tbf.contextCreate()),
+            RxOp.last()
+        )
+        .subscribe({
+            next: res => {
+                testBlock.flush(res)
+            },
+            complete: _ => {
+                stream.destroy()
+            }
+        })
+}
+
+
+const parseCommentMode = argv => argv[2] === '-u'
+    ? CommentMode.uncomment
+    : CommentMode.comment
+
+
+flt.runCmdLineHelper(
+    process.argv,
+    'Lists inline-tests in the file.',
+    nameOfFile =>
+        flt.streamFromFileNameTask(nameOfFile)
+            .map(stream => work(stream, nameOfFile, parseCommentMode(process.argv)))
+            .fork(
+                flt.stdOutErrorHandler,
+                flt.identity
+            )
+)
+
