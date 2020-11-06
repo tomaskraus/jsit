9e840ddc80b920c13ca961608783c985dbee961a
Tomas Kraus <tomas.kraus.dev@gmail.com>
2020-11-06 01:44:52 +0100

    add list-test-full auxiliary tool

---
+++ tools/list-tests-full.js
@@ -0,0 +1,69 @@
+const { compose } = require('folktale/core/lambda')
+const { map } = require('pointfree-fantasy')
+const RxOp = require('rxjs/operators')
+
+const flt = require('../file-line-tools')
+const tbf = require('../text-block-filter')
+
+const { TestBlock } = require('../TestBlock')
+
+
+
+const work = (stream, fileName) => {
+
+    const testBlock = TestBlock.create(
+        compose.all(
+            tbf.Result.Ok, //we also want to show headers
+            tbf.tap(c => console.log(`  --BEGIN line: ${c.lineNum}`)),
+        ),
+
+        compose.all(
+            tbf.Result.Error,
+            tbf.tap(c => console.log(`  ----TEST END`)),
+        )
+    )
+
+    const linesResulter = compose.all(
+        res => res.orElse(
+            c => tbf.Result.Error(
+                tbf.tap(cs => console.log(`${c.lineNum}\t${cs.original}`), c)
+            )
+        ),
+        map(tbf.tap(c => console.log(`${c.lineNum}\t${testBlock.isInCommentBlock(c) ? "*block*" : "       "} ${c.original}`))),
+        //tbf.Result.Ok,
+        testBlock.resulter
+    )
+
+
+    console.log(`processing: ${fileName}`)
+
+    flt.lineObservableFromStream(stream)
+        .pipe(
+            RxOp.scan(tbf.reducer(linesResulter), tbf.contextCreate()),
+            RxOp.last()
+        )
+        .subscribe({
+            next: res => {
+                console.log(
+                    testBlock.flush(res)
+                )
+            },
+            complete: _ => {
+                stream.destroy()
+            }
+        })
+}
+
+
+flt.runCmdLineHelper(
+    process.argv,
+    'Lists inline-tests in the file.',
+    nameOfFile =>
+        flt.streamFromFileNameTask(nameOfFile)
+            .map(stream => work(stream, nameOfFile))
+            .fork(
+                flt.stdOutErrorHandler,
+                flt.identity
+            )
+)
+
